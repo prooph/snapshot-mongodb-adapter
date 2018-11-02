13a44053598d02b730758b168bc04a936a8b672c
Bas Kamer <bas@bushbaby.nl>
2018-11-02 14:25:38 +0100

    apply prooph cs-fixer

--- tests/MongoDbSnapshotAdapterTest.php
+++ tests/MongoDbSnapshotAdapterTest.php
@@ -1,7 +1,9 @@
 <?php
-/*
+
+/**
+ * /*
  * This file is part of the prooph/snapshot-mongodb-adapter.
- * (c) 2014 - 2018 prooph software GmbH <contact@prooph.de>
+ * (c) 2014-2018 - 2018 prooph software GmbH <contact@prooph.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
